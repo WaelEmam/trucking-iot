d5c07371cceffcbb3624a004fc4c28d0e9718d1a
Wael <wael.emam@gmail.com>
2019-04-10 12:54:14 -0400

    fix directory location & sbt packaging command

--- scripts/builds/nifi-bundle.sh
+++ scripts/builds/nifi-bundle.sh
@@ -9,10 +9,10 @@ projVer=$(cat version.sbt | grep '".*"' -o | sed 's/"//g')
 nifiLibDir=$(find / -type d -path "/usr/*/nifi/lib" -print -quit 2> /dev/null)
 
 echo "Building the trucking-nifi-bundle project"
-sbt nifiBundle/compile
+sbt nifiBundle/package
 
 echo "Installing the NiFi nar to NiFi.  NiFi will be restarted"
-cp -f $projDir/trucking-nifi-bundle/nifi-trucking-nar/target/nifi-trucking-nar-$projVer.nar $nifiLibDir
+cp -f $projDir/nifi-bundle/nifi-trucking-nar/target/nifi-trucking-nar-$projVer.nar $nifiLibDir
 
 echo "Restarting NiFi via Ambari"
 curl -u admin:admin -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context": "Stop NIFi"}, "ServiceInfo": {"state": "INSTALLED"}}' http://sandbox-hdf.hortonworks.com:8080/api/v1/clusters/Sandbox/services/NIFI | python $projDir/scripts/wait-until-done.py
